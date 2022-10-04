3f7cdccd516a18332640f02befc3bedc41eff8e3
rasmi <abdelwadoudrasmi@gmail.com>
2022-10-04 13:55:34 +0200

    Initial commit

---
+++ src/main/java/fr/industryportal/ontomapper/OntomapperApplication.java
@@ -0,0 +1,13 @@
+package fr.industryportal.ontomapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntomapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OntomapperApplication.class, args);
+    }
+
+}
