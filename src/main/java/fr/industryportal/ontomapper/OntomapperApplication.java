40f932f3da8b2e5cce82903219394f42a12bd58f
rasmi <abdelwadoudrasmi@gmail.com>
2022-10-04 15:19:16 +0200

    App basic config

--- src/main/java/fr/industryportal/ontomapper/OntomapperApplication.java
+++ src/main/java/fr/industryportal/ontomapper/OntomapperApplication.java
@@ -1,13 +1,18 @@
 package fr.industryportal.ontomapper;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OntomapperApplication {
+public class OntomapperApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(OntomapperApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
