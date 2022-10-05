c723f3c0ec61c1223a6c26dad8181f14d65669e9
rasmi <abdelwadoudrasmi@gmail.com>
2022-10-05 15:12:18 +0200

    Added MappingCardinality enum

---
+++ src/main/java/fr/industryportal/ontomapper/model/entities/embeddables/MappingCardinality.java
@@ -0,0 +1,21 @@
+package fr.industryportal.ontomapper.model.entities.embeddables;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author Abdelwadoud Rasmi
+ * Enumeral values for Entity that is being mapped
+ */
+@AllArgsConstructor
+public enum MappingCardinality {
+    ONE_TO_ONE("1:1"),
+    ONE_TO_MANY("1:n"),
+    MANY_TO_ONE("n:1"),
+    ONE_TO_NONE("1:0"),
+    NONE_TO_ONE("0:1"),
+    MANY_TO_MANY("n:n");
+
+    @Getter
+    private final String value;
+}
