5fed6592688a1021ed56ee5961b4102ff5b0dbff
rasmi <abdelwadoudrasmi@gmail.com>
2022-10-06 15:28:41 +0200

    Refactoring

--- src/main/java/fr/industryportal/ontomapper/model/repos/ContributionRepository.java
+++ src/main/java/fr/industryportal/ontomapper/model/repos/ContributionRepository.java
@@ -2,9 +2,11 @@ package fr.industryportal.ontomapper.model.repos;
 
 import fr.industryportal.ontomapper.model.entities.Contribution;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * @author Abdelwadoud Rasmi
  */
+@RepositoryRestResource
 public interface ContributionRepository extends JpaRepository<Contribution, Long> {
 }
--- src/main/java/fr/industryportal/ontomapper/model/repos/MappingSetRepository.java
+++ src/main/java/fr/industryportal/ontomapper/model/repos/MappingSetRepository.java
@@ -2,9 +2,11 @@ package fr.industryportal.ontomapper.model.repos;
 
 import fr.industryportal.ontomapper.model.entities.MappingSet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * @author Abdelwadoud Rasmi
  */
+@RepositoryRestResource
 public interface MappingSetRepository extends JpaRepository<MappingSet, Long> {
 }
