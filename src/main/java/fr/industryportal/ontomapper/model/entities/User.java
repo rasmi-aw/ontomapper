3cfaf19f4a1239af31511efed45ca40279731ba2
rasmi <abdelwadoudrasmi@gmail.com>
2022-10-06 13:19:09 +0200

    Added user entity to remove redund... of Author,contributor, and reviewer code

---
+++ src/main/java/fr/industryportal/ontomapper/model/entities/User.java
@@ -0,0 +1,29 @@
+package fr.industryportal.ontomapper.model.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * @author Abdelwadoud Rasmi
+ * Entity for a author
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @Getter
+    @Setter
+    private String id;
+
+    @Getter
+    @Setter
+    private String label;
+}
