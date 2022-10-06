17aaef03e872b26e82100770decdcf7120e63e22
rasmi <abdelwadoudrasmi@gmail.com>
2022-10-06 15:05:53 +0200

    Refactored the app, and added repositories

--- src/main/java/fr/industryportal/ontomapper/model/repos/ContributorRepository.java
+++ src/main/java/fr/industryportal/ontomapper/model/repos/ContributorRepository.java
@@ -1,10 +1,10 @@
 package fr.industryportal.ontomapper.model.repos;
 
-import fr.industryportal.ontomapper.model.entities.Author;
+import fr.industryportal.ontomapper.model.entities.Contributor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @author Abdelwadoud Rasmi
  */
-public interface AuthorRepository extends JpaRepository<Author, String> {
+public interface ContributorRepository extends JpaRepository<Contributor, Long> {
 }
