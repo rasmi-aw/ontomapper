97909d086d76f9b0d0cc246023e4fbb149d05310
rasmi <abdelwadoudrasmi@gmail.com>
2022-10-10 11:22:35 +0200

    Removed repos exposed

--- src/main/java/fr/industryportal/ontomapper/model/repos/ContributorRepository.java
+++ src/main/java/fr/industryportal/ontomapper/model/repos/ContributorRepository.java
@@ -2,11 +2,9 @@ package fr.industryportal.ontomapper.model.repos;
 
 import fr.industryportal.ontomapper.model.entities.Contributor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * @author Abdelwadoud Rasmi
  */
-@RepositoryRestResource
 public interface ContributorRepository extends JpaRepository<Contributor, Long> {
 }
