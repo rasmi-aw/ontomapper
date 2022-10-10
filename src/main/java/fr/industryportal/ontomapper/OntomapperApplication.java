8355156050a83da090ad1b332540982cecbde077
rasmi <abdelwadoudrasmi@gmail.com>
2022-10-10 13:43:10 +0200

    Fake data generation algorithm

--- src/main/java/fr/industryportal/ontomapper/OntomapperApplication.java
+++ src/main/java/fr/industryportal/ontomapper/OntomapperApplication.java
@@ -1,8 +1,23 @@
 package fr.industryportal.ontomapper;
 
+import fr.industryportal.ontomapper.model.entities.Contribution;
+import fr.industryportal.ontomapper.model.entities.Contributor;
+import fr.industryportal.ontomapper.model.entities.Mapping;
+import fr.industryportal.ontomapper.model.entities.MappingSet;
+import fr.industryportal.ontomapper.model.entities.enums.ContributorType;
+import fr.industryportal.ontomapper.model.entities.enums.EntityType;
+import fr.industryportal.ontomapper.model.entities.enums.MappingCardinality;
+import fr.industryportal.ontomapper.model.entities.enums.PredicateModifier;
+import fr.industryportal.ontomapper.model.repos.ContributionRepository;
+import fr.industryportal.ontomapper.model.repos.ContributorRepository;
+import fr.industryportal.ontomapper.model.repos.MappingRepository;
+import fr.industryportal.ontomapper.model.repos.MappingSetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 /**
  * @author Abdelwadoud Rasmi
@@ -11,12 +26,49 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OntomapperApplication implements CommandLineRunner {
 
+    @Autowired
+    private MappingSetRepository mappingSetRepository;
+
+    @Autowired
+    private MappingRepository mappingRepository;
+
+    @Autowired
+    private ContributorRepository contributorRepository;
+
+    @Autowired
+    private ContributionRepository contributionRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(OntomapperApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+
+        //Generating fake data
+        for (int i = 0; i < 100; i++) {
+            MappingSet set = mappingSetRepository.save(new MappingSet("set" + i, "version" + i, null, "description" + i, null, "license" + i, EntityType.RDFS_DATATYPE, "", "", EntityType.RDFS_CLASS, "", "", "", "", new Date(), "", "", "", "", "", "", "", new Date(), null));
+            for (int j = 0; j < 1000; j++) {
+                Mapping mapping = mappingRepository.save(new Mapping(
+                        "mapping" + i, "subject" + j, "label" + j, "category" + i, "", "", PredicateModifier.NOT, "", "", "",
+                        "", null, "", EntityType.RDFS_DATATYPE, "", "", EntityType.OWL_CLASS, "", "", "",
+                        "", MappingCardinality.MANY_TO_MANY, "", "", new Date(), 0, "", "", "", "",
+                        "", 0, "", "", "", "", new Date(), set));
+            }
+        }
+
+        for (int i = 0; i < 100; i++) {
+            Contributor contributor = contributorRepository.save(new Contributor("contributor" + i, "label" + i, null));
+            for (int j = 0; j < 1000; j++) {
+                contributionRepository.save(new Contribution(null, contributor, ContributorType.AUTHOR, mappingSetRepository.findAll(), mappingRepository.findAll()));
+                contributionRepository.save(new Contribution(null, contributor, ContributorType.CREATOR, mappingSetRepository.findAll(), null));
+                contributionRepository.save(new Contribution(null, contributor, ContributorType.REVIEWER, mappingSetRepository.findAll(), mappingRepository.findAll()));
+
+            }
+
+        }
+
 
     }
 }
