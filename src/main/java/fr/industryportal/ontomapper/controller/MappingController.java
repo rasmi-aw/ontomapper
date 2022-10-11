08ba3b9d05e5ef54c085a7673de331b9ddf2cc46
rasmi <abdelwadoudrasmi@gmail.com>
2022-10-11 11:04:46 +0200

    Controllers update

--- src/main/java/fr/industryportal/ontomapper/controller/MappingController.java
+++ src/main/java/fr/industryportal/ontomapper/controller/MappingController.java
@@ -6,6 +6,7 @@ import fr.industryportal.ontomapper.model.repos.MappingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,7 +26,7 @@ public class MappingController {
      * get a list of mappings for a specefic set
      */
     @GetMapping("")
-    public List<Mapping> getMappings() {
-        return mappingRepository.findAllBySetIdAndIdBetween(0, 0, 10);
+    public List<Mapping> getFiftyMappings(@RequestParam Long set_id, @RequestParam Long from) {
+        return mappingRepository.findFiftyBySetIdAndIdAndIdAfter(set_id, from);
     }
 }
--- src/main/java/fr/industryportal/ontomapper/controller/MappingSetController.java
+++ src/main/java/fr/industryportal/ontomapper/controller/MappingSetController.java
@@ -1,7 +1,14 @@
 package fr.industryportal.ontomapper.controller;
 
+import fr.industryportal.ontomapper.cache.CacheSet;
+import fr.industryportal.ontomapper.model.entities.MappingSet;
+import fr.industryportal.ontomapper.model.repos.MappingSetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 /**
@@ -11,4 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/set")
 public class MappingSetController {
+
+    @Autowired
+    private MappingSetRepository mappingSetRepository;
+
+    @GetMapping("")
+    public List<MappingSet> getAllSets() {
+        return CacheSet.getInstance(mappingSetRepository).getSets();
+    }
+
 }
