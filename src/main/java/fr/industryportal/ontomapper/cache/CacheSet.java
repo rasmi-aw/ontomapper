3df079a7701d4e9c441f796c4598c809785b03dc
rasmi <abdelwadoudrasmi@gmail.com>
2022-10-11 11:04:22 +0200

    Cache for MappingSet

---
+++ src/main/java/fr/industryportal/ontomapper/cache/CacheSet.java
@@ -0,0 +1,48 @@
+package fr.industryportal.ontomapper.cache;
+
+import fr.industryportal.ontomapper.model.entities.MappingSet;
+import fr.industryportal.ontomapper.model.repos.MappingSetRepository;
+import lombok.Getter;
+
+import java.util.List;
+
+
+/**
+ * @author Abdelwadoud Rasmi
+ * Cached sets
+ */
+public class CacheSet {
+    private static CacheSet instance;
+    private MappingSetRepository mappingSetRepository;
+
+    @Getter
+    private List<MappingSet> sets;
+
+    /**
+     *
+     */
+    public static CacheSet getInstance(MappingSetRepository mappingSetRepository) {
+        if (instance == null)
+            instance = new CacheSet(mappingSetRepository);
+        return instance;
+    }
+
+    /**
+     * Private constructor
+     */
+    private CacheSet(MappingSetRepository mappingSetRepository) {
+        this.mappingSetRepository = mappingSetRepository;
+        sets = mappingSetRepository.findAll();
+    }
+
+    /**
+     * reread mappings from db
+     * This is a heavy operation, but should be fine since sets number is not big
+     */
+    public CacheSet rearm() {
+        sets = mappingSetRepository.findAll();
+        return this;
+    }
+
+
+}
